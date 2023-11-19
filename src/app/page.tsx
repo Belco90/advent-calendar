2d13b7baba2e03df57ae5d386044f530360c12fa
Mario Beltrán Alarcón <belco90@gmail.com>
2023-11-19 11:12:17 +0100

    Retrieve compartments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+import { type Database } from '@/lib/database.types'
+import { Grid } from '@/styled-system/jsx'
+
+async function HomePage() {
+	const cookieStore = cookies()
+	const supabase = createServerComponentClient<Database>({
+		cookies: () => cookieStore,
+	})
+	const { data: compartments } = await supabase.from('compartment').select()
+
 	return (
 		<main>
-			<h1>AC 2023</h1>
+			<h1>Compartments</h1>
+			<Grid columns={3} gap="2">
+				{compartments?.map((compartment) => (
+					<div key={compartment.id}>
+						<div>{compartment.title}</div>
+						<img src={compartment.pictureFK} alt="" />
+					</div>
+				))}
+			</Grid>
 		</main>
 	)
 }
+
+export default HomePage
