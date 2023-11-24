3dff82d04c747f2b2502ba2b563ec790a1efdbe0
Mario Beltrán <belco90@gmail.com>
2023-11-24 13:48:45 +0100

    Check if compartment day is allowed

--- src/app/api/compartment/[id]/open/route.ts
+++ src/app/api/compartment/[id]/open/route.ts
@@ -1,8 +1,26 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { getDate, getMonth } from 'date-fns'
+import MockDate from 'mockdate'
 import { cookies } from 'next/headers'
 import { type NextRequest, NextResponse } from 'next/server'
 
 import { type Database } from '@/lib/database.types'
+
+function getIsCompartmentDayAllowed(day: number): boolean {
+	const mockDateString = process.env.MOCK_DATE
+	if (mockDateString) {
+		MockDate.set(mockDateString)
+	}
+	const today = new Date()
+
+	// Months are 0-index based, so 11 is December.
+	if (getMonth(today) !== 11) {
+		return false
+	}
+	const todayDay = getDate(today)
+
+	return day <= todayDay
+}
 
 export async function POST(
 	request: NextRequest,
@@ -34,7 +52,13 @@ export async function POST(
 		return NextResponse.json({ error: 'Compartment locked.' }, { status: 403 })
 	}
 
-	// TODO: check if the day is allowed to be opened
+	if (!getIsCompartmentDayAllowed(compartment.day)) {
+		return NextResponse.json(
+			{ error: 'Compartment day not allowed.' },
+			{ status: 403 },
+		)
+	}
+
 	// TODO: open the compartment
 
 	return Response.json({ day: compartment.day })
