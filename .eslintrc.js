ae2b3adf7969ad8a046d87686f4eba149e09ac03
Mario Beltrán <belco90@gmail.com>
2023-11-25 19:36:15 +0100

    Disable no-misused-promises ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
 			},
 		],
 		'@typescript-eslint/no-explicit-any': 'error',
+		'@typescript-eslint/no-misused-promises': 'off',
 
 		// Import
 		'import/newline-after-import': 'error',
