b014fc774b5060d3355f5de0a9826f96cfef212c
Mario Beltrán <belco90@gmail.com>
2023-11-28 18:36:35 +0100

    WIP: loading open compartment

---
+++ src/components/FullscreenBackdrop.tsx
@@ -0,0 +1,10 @@
+import { type FC } from 'react'
+
+import { Box } from '@/styled-system/jsx'
+
+const FullscreenBackdrop: FC = () => {
+	// TODO: make it full screen overlay transparent
+	return <Box />
+}
+
+export default FullscreenBackdrop
