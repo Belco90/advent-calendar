d085d5ee4e5402abb28911f2565c1d34201d7743
Mario Beltrán <belco90@gmail.com>
2023-11-29 12:36:53 +0100

    Add loading state for main page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,30 @@
 import { type FC } from 'react'
 
+import { Box, Grid } from '@/styled-system/jsx'
+
+const PLACEHOLDER_ARRAY = Array.from(Array(24), (_, i) => i)
+
 const LoadingPage: FC = () => {
-	return <div>Cargando...</div>
+	return (
+		<Box>
+			<Grid columns={3} gap={{ base: '4', md: '20' }}>
+				{PLACEHOLDER_ARRAY.map((arrKey) => (
+					<Box
+						key={arrKey}
+						height="full"
+						width="full"
+						aspectRatio="square"
+						animation="pulse"
+						bgColor="green.200"
+						borderRadius="2xl"
+						borderColor="green.700"
+						borderStyle="dashed"
+						borderWidth="medium"
+					/>
+				))}
+			</Grid>
+		</Box>
+	)
 }
 
 export default LoadingPage
