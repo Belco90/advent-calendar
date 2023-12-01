3f98c7d3aecba176ece8fd80386cfa91d9b5371d
Mario Beltrán <belco90@gmail.com>
2023-12-01 13:33:20 +0100

    Set generic loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { type FC } from 'react'
+
+import FullscreenBackdrop from '@/components/FullscreenBackdrop'
+
+const RootLoading: FC = () => {
+	return <FullscreenBackdrop />
+}
+
+export default RootLoading
