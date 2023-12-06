913e45e3efe8c3e4d483c35e63aa71c1e32f479a
Mario Beltrán <belco90@gmail.com>
2023-12-06 14:00:03 +0100

    List all compartment table rows

--- src/app/mm17_/page.tsx
+++ src/app/mm17_/page.tsx
@@ -1,7 +1,61 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import Image from 'next/image'
+import Link from 'next/link'
 import { type FC } from 'react'
 
-const AdminIndexPage: FC = () => {
-	return <div>THIS IS THE ADMIN PAGE</div>
+import { type CompartmentTable, type Database } from '@/lib/database.types'
+import { getPicturePublicUrl } from '@/lib/utils'
+import { css } from '@/styled-system/css'
+import { Box, Grid } from '@/styled-system/jsx'
+
+const getAdminCompartments =
+	async (): Promise<Array<CompartmentTable> | null> => {
+		const supabase = createServerComponentClient<Database>({ cookies })
+
+		const { data } = await supabase
+			.from('compartment')
+			.select()
+			.order('day', { ascending: true })
+
+		return data
+	}
+
+const AdminIndexPage: FC = async () => {
+	const compartments = await getAdminCompartments()
+
+	if (!compartments || compartments.length === 0) {
+		return <div>Compartments not found</div>
+	}
+
+	return (
+		<Grid columns={3}>
+			{compartments.map(({ id, pictureFK, day }) => (
+				<Box key={id} aspectRatio="square" position="relative">
+					<Link href={`/mm17_/update/${id}`}>
+						<Image
+							src={getPicturePublicUrl(pictureFK)}
+							alt=""
+							fill
+							className={css({ objectFit: 'cover', rounded: 'lg' })}
+						/>
+					</Link>
+					<Box
+						position="absolute"
+						left="2"
+						top="1"
+						zIndex="overlay"
+						fontWeight="bold"
+						fontSize="4xl"
+						color="green.300"
+						textShadow="md"
+					>
+						{day}
+					</Box>
+				</Box>
+			))}
+		</Grid>
+	)
 }
 
 export default AdminIndexPage
